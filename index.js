c967296a1907394436660016ccb78dfda4aa523c
Gurharpartap Singh <gurharpartapdhaliwal@gmail.com>
2021-06-22 23:32:08 +0530

    Add Sign In and Sign Up page

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express=require('express');
 const path=require('path');
 const port=8000;
+const db=require('./config/mongoose');
+const User=require('./models/user');
+const cookieparser=require('cookie-parser');
 
 const app=express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'Views'));
 
 app.use(express.static('assets'));
+app.use(express.urlencoded());
+app.use(cookieparser());
+
 
 app.get('/',function(req,res){
+    return res.render('home');
+});
+app.post('/create',function(req,res){
+    User.findOne({username:req.body.username},function(err,user){
+        if(err){console.log("Error in finding user in signing up");return ;}
+        if(!user)
+        {
+            User.create(req.body,function(err,user){
+                console.log(user);
+                return res.redirect('/sign-in');
+            });
+        }
+        else
+        {
+            res.redirect('/sign-in');
+        }
+    });
+});
+app.get('/sign-in',function(req,res){
     return res.render('sign_in');
+});
+app.get('/sign-up',function(req,res){
+    return res.render('sign_up');
 });
 
 
